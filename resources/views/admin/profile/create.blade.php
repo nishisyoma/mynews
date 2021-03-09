d50e03dc92a0f695eb832cd173ca79d0c65e88ef
nishisyoma <syouma24free@gmail.com>
2021-03-09 08:08:43 +0000

    laravel12

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,16 @@
+{{-- layouts/admin.bland.phpを読み込む --}}
+    @extends('layouts.profile')
+    
+    {{-- admin.bland.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+    @section('title','Myプロフィール')
+    
+    {{-- admin.bland.phpの@yield('content')に以下のタグを埋め込む --}}
+    @section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h2>Myプロフィール</h2>
+                </div>
+            </div>
+        </div>
+    @endsection
